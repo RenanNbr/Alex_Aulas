c2189bcacf2f8f41dc1c817f550f09cc7b753692
RenanNbr <128818511+RenanNbr@users.noreply.github.com>
2024-04-26 09:41:05 -0300

    Add files via upload

---
+++ exercicios-aulaAlex/8.js
@@ -0,0 +1,8 @@
+// let alert=('Atenção, recomendamos que salve o arquivo ao SAIR');
+
+// console.log(alert)
+
+function alertar(){
+    alert('Atenção, recomendamos que salve o arquivo ao SAIR')
+
+}
